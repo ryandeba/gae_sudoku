a32917c356f595fb4e97cd50ed4771740a9f0d15
ryandeba <ryandeba@gmail.com>
2013-09-02 21:14:17 -0500

    add solve button that makes ajax call to get the first solution

--- app.py
+++ app.py
@@ -8,14 +8,18 @@ class index(webapp2.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.write(template.render(path, {}))
-        
+
 class solveBoard(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Access-Control-Allow-Origin'] = '*'
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.write(json.dumps({'test':self.request.get('test')}))
+
+        boardValues = str(self.request.get('board'))
+        sudokuSolver = SudokuSolver(boardValues)
+        
+        self.response.write(json.dumps([str(sudokuSolver.getFirstSolution())]))
 
 application = webapp2.WSGIApplication([
     ('/', index),
-    ('/test', solveBoard),
+    ('/solveBoard', solveBoard),
 ], debug=True)
